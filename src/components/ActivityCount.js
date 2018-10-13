8116f88e88964721a6ad1c679c8f2abc14762a64
Priyanka Phapale <pbphapale@gmail.com>
2018-10-13 19:46:10 -0400

    Adding react-icons

--- src/components/ActivityCount.js
+++ src/components/ActivityCount.js
@@ -1,4 +1,6 @@
 import '../stylesheets/ui.scss'
+import { FaUmbrella, FaSun, FaCalendarAlt } from 'react-icons/fa'
+import * as FontAwesome from 'react-icons/fa'
 
 const percentToDecimal = (decimal) => {
     return ((decimal * 100) + '%')
@@ -12,14 +14,17 @@ export const ActivityCount = ({ total, rainy, sunny, goal }) => (
     <div className="poker-day-count">
         <div className="total-days">
             <span>{total}</span>
+            <FaCalendarAlt />
             <span>days</span>
         </div>
         <div className="rainy-days">
             <span>{rainy}</span>
+            <FaUmbrella />
             <span>days</span>
         </div>
         <div className="sunny-days">
             <span>{sunny}</span>
+            <FaSun />
             <span>days</span>
         </div>
         <div>
